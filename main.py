411571bb741df7ea95f563717634b7ec2d08eff6
sammysams1234 <162385752+sammysams1234@users.noreply.github.com>
2025-02-12 23:08:24 +0000

    Update and rename main.py to app.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from pages.home import home_page
-
-def main():
-    st.set_page_config(page_title="My Portfolio", layout="wide", initial_sidebar_state="expanded")
-    home_page()
-
-if __name__ == "__main__":
-    main()
