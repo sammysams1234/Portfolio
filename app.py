98c1ae829e863cb6b14e4e1acb455793c073e01f
sammysams1234 <162385752+sammysams1234@users.noreply.github.com>
2025-02-24 16:42:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -351,5 +351,20 @@ I am Samuel Turner, 20, a Level 6 Data Analyst Apprentice at Vodafone, where I h
     st.title("Contact Me")
     st.write("My Email: rebelsrocket910@gmai.com")
     st.write("My Contact No: 07717391987")
+
+    st.markdown ("---")
+    st.title("Complimentary Profile")
+    st.write("While the site covers the majority of this information, feel free to read more about me!")
+
+    def display_pdf(file_path):
+        with open(file_path, "rb") as f:
+            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        pdf_display = f'''
+            <embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">
+        '''
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
+    display_pdf("assets/Samuel Turner OffBoarding Profile.pdf")
+    
 # Run the page
 home_page()
